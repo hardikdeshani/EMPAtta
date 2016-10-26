6ac7ad5a258e359fa9b6697d7baabcc2bc77d28d
dev6websmithsolution <dev6@websmithsolution.com>
2016-10-26 12:05:28 +0530

    helper

---
+++ EMPAttLogic/Helper/MEMBERS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for MEMBERS
+/// </summary>
+public class MEMBERS
+{
+    public struct SQLReturnMessageNValue
+    {
+        public int Outval { get; set; }
+        public string Outmsg { get; set; }
+    }
+
+    public struct SQLReturnValue
+    {
+        public int Outval { get; set; }
+    }
+
+}
