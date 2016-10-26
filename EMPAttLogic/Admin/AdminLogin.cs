89a8221c40adc1be46efbc0d7aecb1c96ed3cedd
dev5@websmithsolution.com <dev5@websmithsolution.com>
2016-10-26 19:01:44 +0530

    a

---
+++ EMPAttLogic/Admin/AdminLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMPAttLogic.Admin
+{
+    public class AdminLogin : SqlHelper
+    {
+        public MEMBERS.SQLReturnMessageNValue ADMIN_AUTH(string UserName, string Password)
+        {
+            return ExecuteProceduerWithMessage("ADMIN_AUTH", new object[,] { { "UserName", UserName }, { "Password", Password } });
+        }
+    }
+}
